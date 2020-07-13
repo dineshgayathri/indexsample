91e8448eefae22d6deb971042481a2ae802c7f12
Your Name <you@example.com>
2020-07-13 13:51:54 +0200

    add subtitle statistics

--- create_download_list.py
+++ create_download_list.py
@@ -3,6 +3,7 @@ import guessit
 import os
 import rank
 import help_routines
+import sys
 
 include_video_ext = set()
 include_video_ext.add('.mkv')
@@ -11,6 +12,10 @@ include_video_ext.add('.m4v')
 include_video_ext.add('.mpg')
 include_video_ext.add('.avi')
 include_video_ext.add('.mpeg')
+
+include_subs_ext = set()
+include_subs_ext.add('.sub')
+include_subs_ext.add('.srt')
 
 client = pymongo.MongoClient()
 cdb1 = client['magnets']
@@ -25,18 +30,53 @@ cdb3 = client['imdb']
 imdb = cdb3.imdb
 
 for document in magnets.find({'files': {'$exists': True}}):
+    to_add = {}
     for file in document['files']:
         path = file['path']
         size = file['size']
         if isinstance(path, list):
-            path = path[0]
-        extension = os.path.splitext(path)[1].lower()
+            path = path[-1]
+        
+        s = os.path.splitext(os.path.basename(path))
+        extension = s[1].lower()
+        key = s[0].lower()
         if extension in include_video_ext:
-            name = os.path.basename(path)
-            info = help_routines.fix_dict(guessit.guessit(name))
+            basename = os.path.basename(path)
+            info = help_routines.fix_dict(guessit.guessit(basename))
+            info.update({
+                'partial_sub': 0,
+                'exact_sub': 0
+            })
+            to_add[key] = {
+                'name': basename,
+                'info': info,
+                'size': size,
+                'path': path
+            }
 
-            if 'season' not in info or 'episode' not in info:
-                value = {'hash': document['_id'], 'name': name, 'size': size, 'extra': info}
+    for file in document['files']:
+        path = file['path']
+        size = file['size']
+        if isinstance(path, list):
+            path = path[-1]
+
+        s = os.path.splitext(os.path.basename(path))
+        extension = s[1].lower()
+        key = s[0].lower()
+        if extension in include_subs_ext:
+            if key in to_add:
+                to_add[key]['info']['exact_sub'] += 1
+                
+            elif len(to_add) == 1:
+                to_add[next(iter(to_add))]['info']['partial_sub'] += 1
+
+            else:
+                pass
+                #print('subtitle ignored')
+
+    for name, data in to_add.items(): 
+            if 'season' not in data['info'] or 'episode' not in data['info']:
+                value = {'hash': document['_id'], 'name': data['name'], 'size': data['size'], 'extra': data['info']}
                 downloads.update_one(
                     {'_id': document['title']},
                     {
@@ -45,10 +85,10 @@ for document in magnets.find({'files': {'$exists': True}}):
                     }, upsert=True
                 )
             else:
-                episodes = help_routines.build_episodes(imdb, document['dbid'], info)
+                episodes = help_routines.build_episodes(imdb, document['dbid'], data['info'])
                 for episode in episodes:
                     key = '%s\t%s' % (document['title'], episode,)
-                    value = {'hash': document['_id'], 'name': name, 'size': size, 'extra': info}
+                    value = {'hash': document['_id'], 'name': data['name'], 'size': data['size'], 'extra': data['info']}
                     downloads.update_one(
                         {'_id': key},
                         {
